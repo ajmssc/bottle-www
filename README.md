f7ecf02478f18883b317681bacb2a4123c8ed569
Jean-Marc <jeanmarc.soumet@gmail.com>
2014-09-12 06:52:25 +0000

    add files

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 bottle-www
 ==========
+
+# Bottle simple project
+This repository provides a simple example of how to use the Bottle python package.
+
+## Install the Bottle Framework
+```
+pip install bottle
+```
+
+
+## Run project
+```
+python project.py
+```
+
+Browse to http://yourserver:8080/hello/test
+
+## TODO:
+Edit the code and add templates to do your API
+
+
+
+# Bottle Framework
+Find more information on Bottle on the [Bottle project homepage](http://bottlepy.org/docs/dev/index.html)
--- project.py
+++ project.py
@@ -1,12 +1,34 @@
 from bottle import route, run, template
+import happybase
+
+hbase = happybase.Connection('localhost')
+my_table = hbase.table('settings')
+
+
+
+
 
 @route('/')
 def index():
 	return template('templates/home', title='my site', base='Welcome!')
 
 
+@route('/api/<key>')
+def api(key):
+	data = my_table.row(key)
+	return data
+
+
+
 @route('/hello/<name>')
 def hello(name):
-    return '<b>Hello %s</b>!' % name
+	return '<b>Hello %s</b>!' % name
+
+
+
+
+
+
+
 
 run(host='0.0.0.0', port=8080)
